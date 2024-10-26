d4567dbb89ce6a41978e58574f269b5d6f055fb2
eliseoinq <153361166+eliseoinq@users.noreply.github.com>
2024-10-26 17:31:33 +0100

    Added Project1_expressions_alert to GitHub

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var A = "Hello People!! "+"How are you all doing?"; //popup alert box  with concataneted text
+window.alert(A);                                    //alert box
+var Team = "Ferrari ", Driver = "Sainz ", principle = "Vasseur "; //Defined variatin family
+document.write(Driver); //variation to be displyed 
+document.write("Hamingway said\"If you are having dessert, you didn't drink enough\"."+"But what if you have desert first?") //concatenated text with backlash function
+document.write(5+5); //actual fuction
